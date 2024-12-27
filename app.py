28c24a1ef53673dd904238395d92b98cfc20718d
Kunchalavanaja <kunchalavanaja799@gmail.com>
2024-12-28 02:57:27 +0530

    values

---
+++ app.py
@@ -0,0 +1,29 @@
+import pickle
+from flask import Flask,request
+
+api=Flask(name)
+
+with open('ai.pkl','rb') as f:
+    ai=pickle.load(f)
+
+@api.route('/')
+def home():
+    return "API Server Running"
+
+@api.route('/predict',methods=['GET'])
+def predict():
+    T=request.args.get('T')
+    T=float(T)
+    P=request.args.get('P')
+    P=float(P)
+    ECG=request.args.get('ECG')
+    ECG=float(ECG)
+    data=[[T,P,ECG]]
+    response=ai.predict(data)[0]
+    return response
+
+if name=="main":
+        api.run(
+            host='0.0.0.0',
+            port=2000
+        )
